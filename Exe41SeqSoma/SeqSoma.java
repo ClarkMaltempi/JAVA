e6f3426fc5a47ca6ccbe7360cb52471f09a1864a
Clark Maltempi <68931244+ClarkMaltempi@users.noreply.github.com>
2022-09-12 10:13:17 -0300

    Update SeqSoma.java

--- Exe41SeqSoma/SeqSoma.java
+++ Exe41SeqSoma/SeqSoma.java
@@ -1,3 +1,5 @@
+//41. Calcular e exibir a soma dos “N” primeiros valores da seqüência abaixo. O valor “N” será digitado, deverá ser positivo, mas menor que cem. Caso o valor não satisfaça a restrição, enviar mensagem de erro e solicitar o valor novamente.
+//A seqüência: 2, 5, 10, 17, 26, ....
 
 import java.util.*;
 
