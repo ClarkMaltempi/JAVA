d0c5460be392159cba041d6a2b7ff0b50e9fda40
Clark Maltempi <68931244+ClarkMaltempi@users.noreply.github.com>
2022-09-12 15:36:56 -0300

    Add files via upload

---
+++ Exe45.java
@@ -0,0 +1,81 @@
+//45. Entrar via teclado com “N” valores quaisquer. O valor “N” 
+//(que representa a quantidade de números) será digitado, deverá ser positivo, mas menor que vinte. 
+//Caso a quantidade não satisfaça a restrição, enviar mensagem de erro e solicitar o valor novamente. Após a digitação dos “N” valores, exibir:
+//a) O maior valor;
+//b) O menor valor;
+//c) A soma dos valores;
+//d) A média aritmética dos valores;
+//e) A porcentagem de valores que são positivos;
+//f) A porcentagem de valores negativos;
+
+
+
+import java.util.Scanner;
+
+public class Exe45 {
+
+	public static void main(String[] args) {
+	
+		
+		int N, num, MaiorVal=0, MenorVal=1000, Soma=0, media=0, i=1;
+		float porcent=0;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		
+		
+		System.out.println("Quantos Numeros deseja digitar? ");
+		N = ler.nextInt();
+		
+		while(N > 20 || N < 0) {
+			System.out.println("ERRO! Digite um numero menor que 50 e positivo");
+			N = ler.nextInt();
+		}
+		
+		float v[] = new float[N];
+		
+		while(i <= N) {
+			
+			System.out.printf("Digite um numero inteiro: ");
+			num = ler.nextInt();
+			
+			Soma+=num;//Soma dos valores
+		
+			porcent = num;
+			if(porcent > 0) {
+				v[i-1] = porcent/100;
+			}
+			
+			if(MaiorVal < num) {
+				MaiorVal = num;
+			}
+			
+			if(num < MenorVal) {
+				MenorVal = num;
+			}
+				
+			
+			i++;
+			
+		}
+		media = Soma/N; //Média 
+		System.out.printf("\nMaior valor: %d",MaiorVal);
+		System.out.printf("\nMenor valor: %d",MenorVal);
+		System.out.printf("\nSoma dos Valores: %d", Soma);
+		System.out.printf("\nMedia: %d", media);
+		
+		i=1;
+		while(i<=N) {
+			
+			System.out.printf("\n\nPorcentagem: %.2f", v[i-1]);
+			
+			i++;
+		}
+		
+		
+		
+		
+
+	}
+
+}
