3736a94e3e32385e7e1ec5dff84c3a68bbd56fe4
Clark Maltempi <68931244+ClarkMaltempi@users.noreply.github.com>
2022-09-13 12:58:01 -0300

    Update Exe43SeqSoma.java

--- Exe43SeqSoma.java
+++ Exe43SeqSoma.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 
+//43. Calcular e exibir a soma dos “N” primeiros valores da seqüência abaixo. 
+//O valor “N” será digitado, deverá ser positivo, mas menor que cinqüenta. 
+//Caso o valor não satisfaça a restrição, enviar mensagem de erro e solicitar o valor novamente.
+//Sequencia: 2 , 5/8 , 10/27, 17/64
+
 public class Exe43SeqSoma {
 
 	public static void main(String[] args) {
 		
 		
 		
-		int N, soma=0, i=1,x=0, NumAnt=2, NumSeq=1, den=0, somaden=0, somaAnt=0;
+		int N, soma=0, i=0,x=0, cont=0, NumAnt=2, NumSeq=1, ValFinal=0;
 		
 		Scanner ler = new Scanner(System.in);
+		
 		
 		
 		System.out.println("Entre com o valor menor que 50 positivo, para exibir a soma da sequencia: ");
@@ -19,39 +25,60 @@ public class Exe43SeqSoma {
 			N = ler.nextInt();
 		}
 		
+		int vetN[] = new int[N];
 		
-		while(x < 1){
+		while(x < N){
+		
+			System.out.printf(" %d ", NumAnt);
+	
+			vetN[x]=NumAnt;
 			
+			//Calcula valor do numerador
+			soma += NumAnt;
+			NumSeq +=2;
+			NumAnt += NumSeq;
 			
-			System.out.printf("%d ,", NumAnt);
+			x++;
+		
+		}
+		
+		System.out.printf(" \n ");
+		
+		
+		System.out.printf(" \n ");
+	
+		x=0;
+		i=1;
+		soma =0;
+		
+		while(x < N){
 			
-			
-			NumSeq +=2;//3
-			NumAnt += NumSeq;//5+3
-			
-			somaden = NumAnt + NumSeq;
-			
-			while(i < N) {
+			if(x > 0) {
 				
-				System.out.printf("\n %d / %d ,", NumAnt, somaden);
-				 
-				NumSeq +=2;
-				den = NumAnt;
-				NumAnt += NumSeq; //10 Numerador
-				somaden = den+NumAnt+somaden+i;//denominador
+				cont=x;
+				//System.out.printf("V: %d ", cont);
 				
-				System.out.printf(" \nSoma anterior %d ,", somaAnt);
+				while(cont > -1) {
+					
+					soma+=vetN[cont];
+					
+					cont--;	
+				}
 				
-
+				System.out.printf(" %d ", soma+=i);
 				
 				i++;
-						
+				
+			}else {
+				
+				System.out.printf("  ");
 			}
 			
+				
 			x++;
 		}
 		
-		
+
 		
 	}
 
