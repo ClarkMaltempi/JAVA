c9eda90047a01c93c75a741c282d84a419b30d31
Clark Maltempi <68931244+ClarkMaltempi@users.noreply.github.com>
2022-09-20 13:01:38 -0300

    Add files via upload

---
+++ Exe13_MaiorVal.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+public class Exe13_MaiorVal {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner (System.in);
+		
+		int a,b,c, i=0;
+		
+		System.out.printf("Digite 3 numeros");
+		
+		System.out.printf("\nprimeiro numero: ");
+		a = ler.nextInt();
+		
+		System.out.printf("\nsegunto numero: ");
+		b = ler.nextInt();
+		
+		System.out.printf("\nterceiro numero: ");
+		c = ler.nextInt();
+		
+		if(a>b && a >c) {
+			System.out.printf("\nO Maior o numero digitado (a): %d ", a);
+		}else if(b>a && b>c){
+			
+			System.out.printf("\nO Maior o numero digitado (b): %d ", b);
+		}else {
+			System.out.printf("\nO Maior o numero digitado (c): %d ", c);
+		}
+		
+		
+		
+
+	}
+
+}
