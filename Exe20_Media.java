dae19a58d909f896eda696f052639fb1bf20b571
Clark Maltempi <68931244+ClarkMaltempi@users.noreply.github.com>
2022-09-20 14:02:26 -0300

    Update Exe20_Media.java

--- Exe20_Media.java
+++ Exe20_Media.java
@@ -1,5 +1,12 @@
 import java.util.*;
 
+//20. Uma escola com cursos em regime semestral realiza duas avaliações durante o semestre e calcula a média do aluno, da seguinte maneira:
+//MEDIA = (P1 + 2.P2) / 3
+//Fazer um programa para entrar via teclado com o valor da primeira nota (P1) 
+//e o programa deverá calcular e exibir quanto o aluno precisa tirar na segunda nota minimamente
+//(P2) para ser aprovado, sabendo que a média de aprovação é igual a cinco.
+
+
 public class Exe20_Media {
 
 	public static void main(String[] args) {
