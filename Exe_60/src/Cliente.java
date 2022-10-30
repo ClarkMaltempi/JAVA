ac6aac170be1fbfa2241bc8594236ff0775b5f35
Clark Maltempi <68931244+ClarkMaltempi@users.noreply.github.com>
2022-10-30 18:58:52 -0300

    Add files via upload

---
+++ Exe_60/src/Cliente.java
@@ -0,0 +1,71 @@
+public class Cliente {
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public ContaBancaria getConta() {
+		return conta;
+	}
+
+	public void setConta(ContaBancaria conta) {
+		this.conta = conta;
+	}
+
+	private int id;
+	private String nome;
+	private int idade;
+	private String email;
+	private ContaBancaria conta;
+	
+	Cliente(){}
+	
+	Cliente(int id,String nome, int idade,String email, ContaBancaria conta){
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+		this.email = email;
+		this.conta = conta;
+	}
+	
+	public void atualizarEmail(String novoEmail){
+		this.email = novoEmail;
+	}
+	
+	public String exibirNomeIdade() {
+		return (this.nome + "-"+this.idade);
+	} 
+	
+	//public String exibirDadosConta() {
+	//return ("\nAg: "+this.conta.agencia+"\nC/C: "+this.conta.numero+"\nSaldo: "+this.conta.saldo);
+	//}
+	
+}
